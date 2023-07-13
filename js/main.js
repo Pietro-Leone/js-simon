1ed0dbb783d5f48e1df5a309ed942701c81b38fa
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-13 14:25:06 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 'use script'
 
-const countdown = document.getElementById("countdown");
+const countDown = document.getElementById("countdown");
 
+const countDownDate = new Date("Jul 14, 2023 09:30:00").getTime();
+
+const x = setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const difference = countDownDate - now;
+
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    countDown.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (difference < 0) {
+        clearInterval(x);
+        countDown.innerHTML = "Lezione iniziata!";
+    }
+}, 1000);
